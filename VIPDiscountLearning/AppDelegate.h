8215b21236d7c5e497aba07d1a8f07b150c3df60
rookieZhou <mmix1009@163.com>
2014-06-11 21:04:06 +0800

    Initial Commit

---
+++ VIPDiscountLearning/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  VIPDiscountLearning
+//
+//  Created by jeff zhou on 6/11/14.
+//  Copyright (c) 2014 jeff.zhou. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ VIPDiscountLearning/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  VIPDiscountLearning
+//
+//  Created by jeff zhou on 6/11/14.
+//  Copyright (c) 2014 jeff.zhou. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
